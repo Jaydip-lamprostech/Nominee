f73e29e61b90e53da24c5d16dc0d83f59d93106f
Sadariya Bhumi <bhumisadariya18@gmail.com>
2022-11-10 13:18:52 +0530

    changes

--- Nominee Frontend/src/components/NomineesList.js
+++ Nominee Frontend/src/components/NomineesList.js
@@ -1,15 +1,72 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-
 import { Nominees } from "./dummyNominees";
-
 import "../styles/nomineeslist.scss";
+import { useAccount } from "wagmi";
+import { ethers } from "ethers";
+import contract from "../artifacts/Main.json";
+export const CONTRACT_ADDRESS = "0x930C70C11A08764A94D6dC3469eC7b66c6e8E0Df";
 
 function NomineesList() {
   const navigate = useNavigate();
   const walletAdd = "0x054ae6107caadc187c304de87365bc52f8c2adb9";
+  const { address, isConnected } = useAccount();
+  const [isLoading, setLoading] = React.useState(true);
+  const [data, setData] = useState([]);
 
-  if (Nominees.length > 0) {
+  const showNominees = async () => {
+    //contract code starts here...............................
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        if (!provider) {
+          console.log("Metamask is not installed, please install!");
+        }
+        const { chainId } = await provider.getNetwork();
+        console.log("switch case for this case is: " + chainId);
+        if (chainId === 80001) {
+          const con = new ethers.Contract(CONTRACT_ADDRESS, contract, signer);
+          const address_array = await con.getNominees(address);
+          // console.log(address_array);
+          for (let i = 0; i < address_array.length; i++) {
+            // console.log(address_array[i]);
+            const nominee_details = await con.getNomineeDetails(
+              address_array[i]
+            );
+            // console.log(nominee_details[0]);
+            // console.log(nominee_details[1]);
+            // console.log(nominee_details[2]);
+            const url = "https://ipfs.io/ipfs/" + nominee_details[2];
+            if (!data.find((item) => nominee_details[0] === item[0])) {
+              data.push([
+                nominee_details[0],
+                nominee_details[1],
+                url,
+                nominee_details[3],
+              ]);
+            }
+          }
+          setData(data);
+          console.log(data);
+          setLoading(false);
+        } else {
+          alert("Please connect to the mumbai test network!");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    //contract code ends here.................................
+  };
+
+  useEffect(() => {
+    showNominees();
+  }, []);
+
+  if (!isLoading) {
     return (
       <>
         <div className="nominees-main">
@@ -23,26 +80,19 @@ function NomineesList() {
               Add Nominee
             </button>
           </div>
-          {Nominees.map((item, key) => {
+          {data.map((item, key) => {
             return (
               <div className="nominees-container" key={key}>
                 <div className="nominees-profile">
-                  <img
-                    src={item.image}
-                    alt="nominee_profile_image"
-                    width="64px"
-                  />
+                  <img src={item[2]} alt="nominee_profile_image" width="64px" />
                 </div>
                 <div className="nominees-details">
-                  <h2>{item.name}</h2>
-                  <p>{item.email}</p>
+                  <h2>{item[0]}</h2>
+                  <p>{item[1]}</p>
                   <p>
-                    {item.walletAdd.substring(0, 6) +
+                    {item[3].substring(0, 6) +
                       "..." +
-                      item.walletAdd.substring(
-                        walletAdd.length - 5,
-                        walletAdd.length
-                      )}
+                      item[3].substring(item[3].length - 5, item[3].length)}
                   </p>
                 </div>
                 <div className="nominees-last">
